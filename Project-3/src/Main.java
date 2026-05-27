2aa4d4f1d14830867d9d01743fb097f7c29c5dee
sunkarakarthikaa <sunkarakarthika@gmail.com>
2026-05-27 16:15:50 +0530

    randomnum

---
+++ Project-3/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args){
+        Random random=new Random();
+        double num;
+        num=random.nextDouble(1,80);
+        System.out.println(num);
+    }
+    
+}
